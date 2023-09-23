0c428bc86b8e1f580c41fb3fa696acc197ce1417
root <root@REVO>
2023-09-23 15:41:08 +0800

    add test setting

--- test_nju_cpol_coupled_dataloader.py
+++ test_nju_cpol_coupled_dataloader.py
@@ -1,10 +1,11 @@
 from utils import nju_cpol_dataloader
-from utils import config, dataset_cpol_dir, dataset_base_dir
+from utils import *
 from tqdm import tqdm
 
 
 if __name__ == '__main__':
-    dataloader = nju_cpol_dataloader.NjuCpolCoupledDataset.dataloader(f'{dataset_cpol_dir}')
-    pbar = tqdm(dataloader)
+    dataloader_train = nju_cpol_dataloader.NjuCpolCoupledDataset.dataloader(dataset_cpol_dir_train)
+    dataloader_test = nju_cpol_dataloader.NjuCpolCoupledDataset.dataloader(dataset_cpol_dir_test)
+    pbar = tqdm(dataloader_train)
     for batch in pbar:
-        print(batch.shape)
+        pbar.set_description(f"x: {batch[0].shape} y: {batch[1].shape}")
--- utils/__init__.py
+++ utils/__init__.py
@@ -4,4 +4,6 @@ import os.path
 config = configparser.ConfigParser()
 config.read('config/secrets.ini')
 dataset_base_dir = config['dataset']['base_dir']
-dataset_cpol_dir = os.path.join(config['dataset']['base_dir'], config['dataset']['kdprain'])
+dataset_cpol_dir = os.path.join(config['dataset']['base_dir'], config['dataset']['kdprain_train'])
+dataset_cpol_dir_train = dataset_cpol_dir
+dataset_cpol_dir_test = os.path.join(config['dataset']['base_dir'], config['dataset']['kdprain_test'])
